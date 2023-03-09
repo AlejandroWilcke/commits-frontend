2c0263cf0c51027d852b35abf1c9e620315e1138
Alejandro Wilcke <aleee9208@hotmail.com>
2023-03-09 19:12:59 -0300

    feat: Updated CommitList component for using CommitCard

--- components/CommitList.component.tsx
+++ components/CommitList.component.tsx
@@ -1,11 +1,20 @@
+import CommitCard, { ICommit } from "./CommitCard.component";
+
 interface ICommitListProps {
+  repo: string;
+  commits: ICommit[];
   classes?: string;
 }
 
-export default function CommitList({ classes }: ICommitListProps){
+export default function CommitList({ repo, commits, classes }: ICommitListProps){
   return(
-    <ul className={`border-4 border-white rounded-md p-4 my-8 mx-4 ${classes}`}>
-      <li>Base</li>
+    <ul className={`rounded-md p-4 my-8 mx-4 bg-gray-700 h-[50vh] overflow-y-auto scroll-smooth shadow-inner shadow-[#222222] ${classes}`}>
+      <li className="text-center text-white text-xl">Repository: {repo}</li>
+      {commits?.map( (commit, i) => {
+        return (
+          <CommitCard key={i} commit={commit} />
+        )
+      })}
     </ul>
   )
 }
