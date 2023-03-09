6beb55b11fcb98b0c29398416841a1ccdd2d6a10
Alejandro Wilcke <aleee9208@hotmail.com>
2023-03-09 19:13:56 -0300

    feat: Main page renders 2 CommitList

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,34 @@
 import Head from 'next/head'
 import CommitList from '@/components/CommitList.component'
+import { getCommitList } from './api/commit';
+import { ICommit } from '@/components/CommitCard.component';
+import { useQuery } from "react-query";
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+
+  const backendRepo = process.env.NEXT_PUBLIC_BACKEND_REPO || "";
+  const frontendRepo = process.env.NEXT_PUBLIC_FRONTEND_REPO || "";
+
+  const [backendCommits, setBackendCommits] = useState<ICommit[]>([]);
+  const [frontendCommits, setFrontendCommits] = useState<ICommit[]>([]);
+
+  const { 
+    data: backendData,
+  } = useQuery(['repo', backendRepo], () => getCommitList(backendRepo));
+
+  const { 
+    data: frontendData,
+  } = useQuery(['repo', frontendRepo], () => getCommitList(frontendRepo));
+
+  useEffect(() => {
+    setBackendCommits(backendData);
+  }, [backendData]);
+
+  useEffect(() => {
+    setFrontendCommits(frontendData);
+  }, [frontendData]);
+
   return (
     <>
       <Head>
@@ -9,17 +36,14 @@ export default function Home() {
         <meta name="description" content="Commits List" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/github.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200&display=swap" rel="stylesheet"></link>
       </Head>
       <main>
         <h1 className="p-4 text-3xl text-white text-center font-bold font-roboto underline tracking-wider underline-offset-8">
           Github Commits List
         </h1>
         <div className='grid grid-cols-12 mx-auto px-12 lg:px-16 xl:px-40'>
-          <CommitList classes='col-span-12 lg:col-span-6'/>
-          <CommitList classes='col-span-12 lg:col-span-6'/>
+          <CommitList repo={backendRepo} commits={backendCommits} classes='col-span-12 lg:col-span-6'/>
+          <CommitList repo={frontendRepo} commits={frontendCommits} classes='col-span-12 lg:col-span-6'/>
         </div>
       </main>
     </>
